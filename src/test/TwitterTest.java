d6db7209b648d71020b4297b63957f388d1b352e
filipkaric <kfilip94@gmail.com>
2015-04-08 19:00:46 +0200

    Napisan JUnit za klasu Twitter i otkrivene greske u metodama.

---
+++ src/test/TwitterTest.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.twitter.Twitter;
+import com.twitter.poruke.TwitterPoruka;
+
+/**
+ * @author Filip
+ * 
+ */
+public class TwitterTest {
+
+	Twitter tw;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		tw = new Twitter();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		tw = null;
+	}
+
+	@Test
+	public void testVratiSvePoruke() {
+		tw.unesi("Filip", "Ja volim sendvic.");
+		assertEquals("KORISNIK:Filip PORUKA:Ja volim sendvic.", tw
+				.vratiSvePoruke().getLast().toString());
+
+	}
+
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testUnesi() {
+		tw.unesi("Filip", "OMG");
+		assertEquals("Filip", tw.vratiSvePoruke().getLast().getKorisnik());
+		assertEquals("OMG", tw.vratiSvePoruke().getLast().getPoruka());
+		tw.unesi(null, null);
+	}
+	@Test
+	public void testVratiPoruke() {
+		
+		tw.unesi("Filip", "Opet slusam whoSee Dje se kupas?");
+		tw.unesi("Darko", "Opet sam pao ispit :(");
+		
+		TwitterPoruka[] a = tw.vratiPoruke(2, "Opet");
+		assertEquals("KORISNIK:Filip PORUKA:Opet slusam whoSee Dje se kupas?",a[0].toString());
+		assertEquals("KORISNIK:Darko PORUKA:Opet sam pao ispit :(",a[0].toString());
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
